e51f3159c0f857283466b6d187e03040fb82b6e8
neugomonov <neugomonovv@gmail.com>
2022-11-25 19:33:58 +0300

    💀

--- App.tsx
+++ App.tsx
@@ -4,10 +4,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>
-        Open up App.tsx to start working on your app! Obama, hamburger, sussy
-        balls!
-      </Text>
+      <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
