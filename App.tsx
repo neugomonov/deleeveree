afbef4a4ae0dac39f82f225ca11db3db7b8bfd3e
neugomonov <neugomonovv@gmail.com>
2022-11-25 20:22:12 +0300

    ✨ App.tsx - tailwind (styling)

--- App.tsx
+++ App.tsx
@@ -1,12 +1,15 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import { TailwindProvider } from "tailwind-rn";
+import CustomersScreen from "./screens/CustomersScreen";
+import utilities from "./tailwind.json";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    // @ts-ignore - Type '{ children: Element; utilities: {}; }' is not assignable to type 'IntrinsicAttributes & Props'.  Property 'children' does not exist on type 'IntrinsicAttributes & Props'.ts(2322) [TailwindProvider is missing a type definition]
+    <TailwindProvider utilities={utilities}>
+      <CustomersScreen />
+    </TailwindProvider>
   );
 }
 
