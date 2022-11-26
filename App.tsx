1c14483886d771dcddd1a59dc702e9bc2c3bd129
neugomonov <neugomonovv@gmail.com>
2022-11-26 19:24:38 +0300

    📱 App.tsx - screens w/ navigation (added)

--- App.tsx
+++ App.tsx
@@ -1,14 +1,16 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { StyleSheet } from "react-native";
 import { TailwindProvider } from "tailwind-rn";
-import CustomersScreen from "./screens/CustomersScreen";
+import RootNavigator from "./navigator/RootNavigator";
 import utilities from "./tailwind.json";
 
 export default function App() {
   return (
     // @ts-ignore - Type '{ children: Element; utilities: {}; }' is not assignable to type 'IntrinsicAttributes & Props'.  Property 'children' does not exist on type 'IntrinsicAttributes & Props'.ts(2322) [TailwindProvider is missing a type definition]
     <TailwindProvider utilities={utilities}>
-      <CustomersScreen />
+      <NavigationContainer>
+        <RootNavigator />
+      </NavigationContainer>
     </TailwindProvider>
   );
 }
